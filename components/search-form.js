b4bca7e6469572aa77c4a7f384a31e099e9f3e64
luhart <ljhartman10@gmail.com>
2020-09-20 18:56:11 -0700

    job table displays SearchForm state

--- components/search-form.js
+++ components/search-form.js
@@ -1,9 +1,12 @@
-import { Button, Form } from 'react-bootstrap'
+import { Button, Card, Form, Table } from 'react-bootstrap'
 
 export default class SearchForm extends React.Component{
     constructor(props) {
         super(props)
-        this.state = {value: ''}
+        this.state = {
+            value: '',
+            jobs: [],
+        }
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -17,19 +20,54 @@ export default class SearchForm extends React.Component{
         event.preventDefault(); // stops refresh
         fetch('/api/?seq=' + this.state.value)
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then(data => console.log(data))
+            .then(data => this.setState({jobs: this.state.jobs.push(data)}));
         console.log("submitting dna search for: " + this.state.value) 
         this.setState({"value": ""})
     }
 
     render() {
         return (
-            <Form onSubmit={async e => {this.handleSubmit(e)}}>
-                <Form.Group controlId="sequenceForm.TextArea">
-                    <Form.Control as="textarea" rows="2" value={this.state.value} onChange={this.handleChange}/>
-                </Form.Group>
-                <Button variant="outline-primary" type="submit">Search</Button>
-            </Form>
+            <>
+                <section>
+                    <Card>
+                        <Card.Header>
+                            Enter the sequence you'd like to search for
+                        </Card.Header>
+                        <Card.Body>
+                            <Form onSubmit={async e => {this.handleSubmit(e)}}>
+                                <Form.Group controlId="sequenceForm.TextArea">
+                                    <Form.Control as="textarea" rows="2" value={this.state.value} onChange={this.handleChange}/>
+                                </Form.Group>
+                                <Button variant="outline-primary" type="submit">Search</Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                    <br/>
+                </section>
+
+                <section>
+                    <h4>Jobs</h4>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>status</th>
+                                <th>sequence</th>
+                                <th>protein</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.jobs.map((job, index) => (
+                                <tr key={index}>
+                                    <td>{job.status}</td>
+                                    <td>{job.seq}</td>
+                                    <td>{job.protein}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </section>
+            </>
         )
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import SearchForm from '../components/search-form'
 
-import {Button, Card, Table} from "react-bootstrap"
 
 export default function Home() {
   return (
@@ -17,43 +16,7 @@ export default function Home() {
         <h3>A utility for finding sequence alignments in a dna library</h3>
         <br/>
       </section>
-
-      <section>
-        <Card>
-          <Card.Header>
-              Enter the sequence you'd like to search for
-          </Card.Header>
-          <Card.Body>
-            <SearchForm />
-          </Card.Body>
-        </Card>
-        <br/>
-      </section>
-
-      <section>
-        <h4>Jobs</h4>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>sequence</th>
-              <th>status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>0</td>
-              <td>actgaactg</td>
-              <td><a href="google.com"></a>found</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>actgaaag</td>
-              <td>searching...</td>
-            </tr>
-          </tbody>
-        </Table>
-      </section>
+      <SearchForm />
     </Layout>
   )
 }
